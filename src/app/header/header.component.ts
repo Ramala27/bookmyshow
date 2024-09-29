129ebff7b6a9a8d21593a4127e7d29103517fe2b
Ramala27 <rathikapanneerselvam94@gmail.com>
2024-09-30 00:09:49 +0530

    Added logout funcionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  // isLoggedIn = true;
+
+  isLoggedIn = false;
+
+  constructor(){
+    console.log('HeaderComponent');
+    
+
+  }
+  ngOnInit(): void {
+    // fetch form localstorage whether user is loggedin
+    this.isLoggedIn = localStorage.getItem("LOGGED_IN_USER")== 'true' ;
+  }
+
+  logout(){
+    // localStorage.removeItem("LOGGED_IN_USER");
+    localStorage.clear();
+     alert("Successfully LoggedOut`````");
+     window.location.href="/login";
+  }
 
 }
